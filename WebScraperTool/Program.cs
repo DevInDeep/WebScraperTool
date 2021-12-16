93f944573fb3f07edace87477de82652c0d8e908
vpavlevski <vpavlevski@yahoo.com>
2021-12-16 17:07:45 +0100

    Code Cleanup

--- WebScraperTool/Program.cs
+++ WebScraperTool/Program.cs
@@ -6,22 +6,18 @@ namespace WebScraperTool
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main() =>
             Console.WriteLine($"The team with most NBA Championships is: {GetBestNbaTeam()}");
-            Console.ReadLine();
-        }
 
-        static NbaTeam GetBestNbaTeam()=>
-            new WebClient().DownloadWebPage(new Uri("https://en.wikipedia.org/wiki/National_Basketball_Association"))
-            .SelectNodes("//table[@class='wikitable']/tbody//tr/td/b")
+        static NbaTeam GetBestNbaTeam() =>
+            new WebClient().DownloadWebPage(new Uri(Wikipedia.NbaTeamsUrl))
+            .SelectNodes(Wikipedia.NbaTeamsTableXPath)
             .Select(teamNode =>
                     NbaTeam.Create(
                         teamNode.InnerText,
-                        new WebClient().DownloadWebPage(new Uri(CreateUrl(teamNode.SelectSingleNode("a").Attributes["href"].Value)))
-                        .SelectSingleNode("//th[text()='Championships']/following-sibling::td/b").InnerText.ToInt()))
+                        new WebClient()
+                        .DownloadWebPage(Wikipedia.CreateUrl(teamNode.SelectSingleNode("a").Attributes["href"].Value))
+                        .SelectSingleNode(Wikipedia.NbaTeamChampionshipsXPath).InnerText.ToInt()))
             .Aggregate(NbaTeam.Create(), (best, next) => next.Championships > best.Championships ? next : best);
-
-        private static string CreateUrl(string partialUrl) => string.Format("{0}{1}", "https://en.wikipedia.org", partialUrl);
     }
 }
---
+++ WebScraperTool/Wikipedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebScraperTool
+{
+    public class Wikipedia
+    {
+        public const string NbaTeamsUrl = "https://en.wikipedia.org/wiki/National_Basketball_Association";
+        public const string NbaTeamsTableXPath = "//table[@class='wikitable']/tbody//tr/td/b";
+        public const string NbaTeamChampionshipsXPath = "//th[text()='Championships']/following-sibling::td/b";
+
+        public static Uri CreateUrl(string partial) =>
+            new Uri(string.Format("{0}{1}", "https://en.wikipedia.org", partial));
+    }
+}
